9c0fd0703ac9fcf397d9e0b1bef54374fa29335a
Melanie Qu <melaniequ@Melanies-Mac-mini.local>
2026-06-07 19:53:08 -0400

    resolve merge conflict

--- src/data/Preprocessing.py
+++ src/data/Preprocessing.py
@@ -1,66 +1,34 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "e9696e5c-0368-40b6-ba1f-37aa375b5fcd",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import pandas as pd\n",
-    "\n",
-    "import matplotlib.pyplot as plt\n",
-    "import seaborn as sns\n",
-    "\n",
-    "def replace_outliers_with_median(df, column):\n",
-    "    \"\"\"\n",
-    "    Replace outliers in a column using the IQR method.\n",
-    "    Outliers are replaced with the column median.\n",
-    "    \"\"\"\n",
-    "\n",
-    "    Q1 = df[column].quantile(0.25)\n",
-    "    Q3 = df[column].quantile(0.75)\n",
-    "\n",
-    "    IQR = Q3 - Q1\n",
-    "\n",
-    "    lower_bound = Q1 - 1.5 * IQR\n",
-    "    upper_bound = Q3 + 1.5 * IQR\n",
-    "\n",
-    "    median = df[column].median()\n",
-    "\n",
-    "    df.loc[df[column] < lower_bound, column] = median\n",
-    "    df.loc[df[column] > upper_bound, column] = median\n",
-    "\n",
-    "    return df\n",
-    "\n",
-    "\n",
-    "survey_columns = ['X1', 'X2', 'X3', 'X4', 'X5', 'X6']\n",
-    "\n",
-    "for col in survey_columns:\n",
-    "    df = replace_outliers_with_median(df, col)\n",
-    "\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.13.9"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import pandas as pd
+import os
+
+os.makedirs("data/processed", exist_ok=True)
+
+df = pd.read_csv('data/raw/ACME-HappinessSurvey2020.csv')
+
+def replace_outliers_with_median(df, column):
+    """
+    Replace outliers in a column using the IQR method.
+    Outliers are replaced with the column median.
+    """
+
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+
+    IQR = Q3 - Q1
+
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    median = df[column].median()
+
+    df.loc[df[column] < lower_bound, column] = median
+    df.loc[df[column] > upper_bound, column] = median
+
+    return df
+
+survey_columns = ['X1', 'X2', 'X3', 'X4', 'X5', 'X6']
+
+for col in survey_columns:
+    df = replace_outliers_with_median(df, col)
+ 
+df.to_csv('data/processed/ACME-HappinessSurvey2020.csv', index=False)
