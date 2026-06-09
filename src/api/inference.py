5bd812a7dbea6fbee018450df04acbb7d00181cd
bqsauce-ux <bqsauce@gmail.com>
2026-06-09 15:03:09 -0400

    Create inference.py

---
+++ src/api/inference.py
@@ -0,0 +1,18 @@
+import joblib
+import pandas as pd
+from datetime import datetime
+from schemas import CustomerHappinessRequest, PredictionResponse
+
+MODEL_PATH = "src/models/best_model.joblib"
+model = joblib.load(MODEL_PATH)
+
+# load your trained model
+
+def predict_happiness(request: CustomerHappinessRequest -> PredictionResponse:
+    
+    df = pd.DataFrame([data])
+    prediction = model.predict(df)[0]
+
+    return PredictionResponse(
+        predicted_happiness= prediction
+    )
